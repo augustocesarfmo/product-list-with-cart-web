6a04d8a7a305a5db84e15cc93c5a7b58c7bcc363
Augusto Oliveira <augustocesarfmo@gmail.com>
2024-09-23 10:35:38 -0300

    feat: implementa a classe `Cart`

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
 import data from "../data.json";
+import { Cart } from "./entities/cart";
 import { Product } from "./entities/product";
 
-for (const product of data) {
-  new Product(
-    product.name,
-    product.price,
-    product.category,
-    product.image.desktop
-  );
-}
+// for (const product of data) {
+//   new Product(
+//     product.name,
+//     product.price,
+//     product.category,
+//     product.image.desktop
+//   );
+// }
+
+const product1 = new Product(
+  data[0].name,
+  data[0].price,
+  data[0].category,
+  data[0].image.desktop
+);
+
+const product2 = new Product(
+  data[1].name,
+  data[1].price,
+  data[1].category,
+  data[1].image.desktop
+);
+
+const cart = new Cart();
+
+cart.addToCart(product1);
+cart.addToCart(product1);
+
+console.log(cart.products);
+console.log(cart.products.length);
+console.log(cart.total);
--- src/entities/product.ts
+++ src/entities/product.ts
@@ -6,11 +6,28 @@ export class Product {
   private _price: number;
   private _category: string;
   private _imageUrl: string;
+  private _quantity: number = 0;
 
   constructor(name: string, price: number, category: string, imageUrl: string) {
     this._name = name;
     this._price = price;
     this._category = category;
     this._imageUrl = imageUrl;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  set quantity(quantity: number) {
+    this._quantity += quantity;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get price() {
+    return this._price;
   }
 }
